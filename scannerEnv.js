f438ff0d25e1b364fc5efdd39da44d5116a294e9
denisgandel <denis.gandarillas.9355205@usalesiana.edu.bo>
2023-11-10 20:15:37 -0400

    Dev fix

--- scannerEnv.js
+++ scannerEnv.js
@@ -1,15 +1,21 @@
-let messagePage = document.querySelector('.environment-title')
-let url = window.location.hostname
+let messagePage = document.querySelector('.environment-title');
+let url = window.location.hostname;
 
-let keyWords = ['test', 'dev']
+let keyWords = ['test', 'dev'];
 
 let isTestEnv = keyWords.some(function(word) {
-    let regex = new RegExp('\\b' + word + '\\b', 'i')
-    return regex.test(url)
-})
+    let regex = new RegExp('\\b' + word + '\\b', 'i');
+    return regex.test(url);
+});
 
 if (isTestEnv) {
-    messagePage.innerHTML = "IT IS TEST ENVIRONMENT"
+    messagePage.innerHTML = "IT IS TEST ENVIRONMENT";
 } else {
-    messagePage.innerHTML = "IT IS DEV ENVIRONMENT OR YOUR LOCALHOST"
+    let isDevEnv = keyWords.includes(url.toLowerCase());
+
+    if (isDevEnv) {
+        messagePage.innerHTML = "IT IS DEV ENVIRONMENT";
+    } else {
+        messagePage.innerHTML = "IT IS NEITHER TEST NOR DEV ENVIRONMENT";
+    }
 }
